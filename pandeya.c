1bb22d2fc43cacdfd58b65294cf23da76191bf27
Ashok Nath <38045258+ashknth@users.noreply.github.com>
2019-01-28 15:18:52 +0545

    Add files via upload

---
+++ pandeya.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+switch case()
+int main(){
+char c;
+printf("enter number for day");
+scanf("%d",&c);
+switch(day){
+case'+':printf("addition");
+break;
+case'-'printf("tuesday");
+break;
+case'*'printf("wednesday");
+break;
+case'/'printf("thrusday");
+break;
+default:
+printf("invalid");
+break;
+}
+}
